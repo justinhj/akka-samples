ca6687fb6f44572747865d405e0ad75b79d89df3
Justin Heyes-Jones <justin.heyes-jones@yoppworks.com>
2020-04-15 11:34:19 -0700

    add box and tests

---
+++ akka-sample-persistence-scala/src/main/scala/sample/persistence/ItemBox.scala
@@ -0,0 +1,78 @@
+package sample.persistence
+
+// this box will be able to accept objects as long as is not full. So a maxCapacity should be included it's state.
+// expected behavior:
+// should be possible addItem, such as Item(description: String, size: Int)
+// should not be possible addItem, if
+// maxCapacity is already surpassed or the object to add surpasses it.
+// after adding an item it should get back info about how much it still can hold
+//
+
+
+import akka.actor.typed.Behavior
+import akka.persistence.typed.PersistenceId
+import akka.persistence.typed.scaladsl.Effect
+import akka.persistence.typed.scaladsl.EventSourcedBehavior
+import akka.actor.typed.ActorRef
+
+object ItemBox {
+  sealed trait Command
+
+  case class Item(description: String, size: Int)
+
+  val maxObjects = 10
+
+  object State {
+    def empty = State(maxObjects, List.empty[Item])
+  }
+
+  final case class State(capacity: Int, items: List[Item]) {
+    def utilized = items.map(_.size).sum
+    def remainingRoom = capacity - utilized
+  }
+
+  final case class AddItem(boxId: String, item: Item, replyTo: ActorRef[Confirmation]) extends Command
+
+  sealed trait Confirmation extends CborSerializable
+
+  final case class ItemAccepted(remainingRoom: Int) extends Confirmation
+  final case class ItemCannotFit(capacity: Int) extends Confirmation
+
+  sealed trait Event extends CborSerializable {
+    def boxId: String
+  }
+  final case class ItemAdded(boxId: String, item: Item) extends Event
+
+  def itemFits(state: State, item: Item): Boolean = {
+    state.capacity >= item.size
+  }
+
+  def getRemainingRoom(state: State): Int = state.capacity
+
+  def apply(boxId: String): Behavior[Command] = {
+    EventSourcedBehavior[Command, Event, State](PersistenceId("Box", boxId),
+      State.empty,
+      (state, command) => {
+        command match {
+          case AddItem(id, item, replyTo) => {
+            if(!itemFits(state, item)) {
+              replyTo ! ItemCannotFit(getRemainingRoom(state))
+              Effect.none
+            } else {
+              Effect
+                .persist(ItemAdded(id, item))
+                .thenRun(updatedBox => {
+                  replyTo ! ItemAccepted(getRemainingRoom(updatedBox) - item.size)
+                })
+            }
+          }
+        }
+      },
+      (state, event) => {
+          event match {
+            case ItemAdded(id, item) =>
+              state.copy(items = state.items :+ item)
+          }
+        })
+  }
+}
---
+++ akka-sample-persistence-scala/src/test/scala/sample/persistence/ItemBoxSpec.scala
@@ -0,0 +1,64 @@
+package sample.persistence
+
+import java.util.UUID
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class ItemBoxSpec extends ScalaTestWithActorTestKit(s"""
+      akka.persistence.journal.plugin = "akka.persistence.journal.inmem"
+      akka.persistence.snapshot-store.plugin = "akka.persistence.snapshot-store.local"
+      akka.persistence.snapshot-store.local.dir = "target/snapshot-${UUID.randomUUID().toString}"
+    """) with AnyWordSpecLike {
+
+  private var counter = 0
+  def newBoxId(): String = {
+    counter += 1
+    s"box-$counter"
+  }
+
+  "The Item Box" should {
+
+    "add item" in {
+      val box = testKit.spawn(ItemBox(newBoxId()))
+      val probe = testKit.createTestProbe[ItemBox.Confirmation]
+      box ! ItemBox.AddItem("test item 1", ItemBox.Item("Black Socks", 5), probe.ref)
+      probe.expectMessage(ItemBox.ItemAccepted(5))
+    }
+
+    "reject no room for item" in {
+      val box = testKit.spawn(ItemBox(newBoxId()))
+      val probe = testKit.createTestProbe[ItemBox.Confirmation]
+      box ! ItemBox.AddItem("test item 2", ItemBox.Item("Giant Black Socks", 12), probe.ref)
+      probe.expectMessage(ItemBox.ItemCannotFit(10))
+    }
+
+    "add multiple items" in {
+      val box = testKit.spawn(ItemBox(newBoxId()))
+      val probe = testKit.createTestProbe[ItemBox.Confirmation]
+
+      (1 to 10).foreach {
+        n =>
+          box ! ItemBox.AddItem(s"test item s", ItemBox.Item("Single Black Socks", 1), probe.ref)
+          probe.expectMessage(ItemBox.ItemAccepted(10 - n))
+      }
+    }
+
+    // "keep its state" in {
+    //   val cartId = newCartId()
+    //   val cart = testKit.spawn(ShoppingCart(cartId))
+    //   val probe = testKit.createTestProbe[ShoppingCart.Confirmation]
+    //   cart ! ShoppingCart.AddItem("foo", 42, probe.ref)
+    //   probe.expectMessage(ShoppingCart.Accepted(ShoppingCart.Summary(Map("foo" -> 42), checkedOut = false)))
+
+    //   testKit.stop(cart)
+
+    //   // start again with same cartId
+    //   val restartedCart = testKit.spawn(ShoppingCart(cartId))
+    //   val stateProbe = testKit.createTestProbe[ShoppingCart.Summary]
+    //   restartedCart ! ShoppingCart.Get(stateProbe.ref)
+    //   stateProbe.expectMessage(ShoppingCart.Summary(Map("foo" -> 42), checkedOut = false))
+    // }
+  }
+
+}
